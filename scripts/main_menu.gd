8cd29ae1eacc3ce2632edc0d76ecedd7ed4d4acd
thoughtRock05 <thatoneduck07@hotmail.com>
2026-09-18 11:27:58 -0400

    add window options

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,13 +4,18 @@ extends Node2D
 @export var options_scene: StringName = &""
 @export var play_button: Button
 @export var options_button: Button
+@export var quit_button: Button
 
 func _ready() -> void:
 	play_button.pressed.connect(_on_play_button_pressed)
 	options_button.pressed.connect(_on_options_menu_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
 
 func _on_play_button_pressed() -> void:
 	SceneTransition.load_scene(initial_scene)
 
 func _on_options_menu_button_pressed() -> void:
 	SceneTransition.load_scene(options_scene)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
--- scripts/options.gd
+++ scripts/options.gd
@@ -2,9 +2,18 @@ extends Node2D
 
 @export var main_menu: StringName = &""
 @export var back_button: Button
+@export var window_dropdown: OptionButton
 
 func _ready() -> void:
-	back_button.pressed.connect(_on_button_pressed)
+	back_button.pressed.connect(_on_back_button_pressed)
+	window_dropdown.item_selected.connect(_on_window_dropdown_selected)
 
-func _on_button_pressed() -> void:
+func _on_back_button_pressed() -> void:
 	SceneTransition.load_scene(main_menu)
+
+func _on_window_dropdown_selected(i: int) -> void:
+	match i:
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
